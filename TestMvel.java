0fc3bff7cae0f121a8448ef32df2b0c616590fd0
MinBaka <min@minawa.top>
2026-05-27 01:06:30 +0800

    减去无意义内容

--- TestMvel.java
+++
@@ -1,8 +0,0 @@
-import java.util.Map;
-import java.util.HashMap;
-
-public class TestMvel {
-    public static void main(String[] args) throws Exception {
-        // Can't easily test without classpath
-    }
-}
